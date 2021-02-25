882b6b2e2c909ea9ca1b451931bb3e19d6b57870
Nastya Kvak <nastya.kvak@codeimmersives.com>
2021-02-25 14:53:32 -0500

    Adds front-end APP

--- color-cleaver.js
+++ color-cleaver.js
@@ -5,3 +5,13 @@ const isValidSecondary = require("./is-valid-secondary.js");
 const getInput = require("./get-input.js");
 
 // Your code here!
+const col1 = getInput(1);
+const col2 = getInput(2);
+
+if (col1 === undefined && col2 === undefined) {
+    console.log("Please, enter at least one color.")
+} else if (isValidSecondary(col1) === true) {
+    console.log(colorDeconstructor(col1))
+} else {
+    console.log(colorCombinator(col1, col2))
+}
